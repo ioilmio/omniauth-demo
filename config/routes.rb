9d37167096b8eec1fc3e7e9f182e170531f7f1aa
ioilmio <illuminato.salvatore@gmail.com>
2020-09-16 01:03:30 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root to: 'posts#index'
