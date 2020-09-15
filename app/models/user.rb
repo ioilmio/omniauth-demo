c1de4036cd2e3a6856335e5587f912ba2db6c28e
ioilmio <illuminato.salvatore@gmail.com>
2020-09-15 23:22:39 +0200

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,  :omniauthable, omniauth_providers: %i[github]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
   has_many :posts
 end
